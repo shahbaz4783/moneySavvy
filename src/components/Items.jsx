760593ac9f27c5d56d05f18274c319fc075233a2
shahbaz4783 <shahbaz4783@gmail.com>
2023-08-16 20:18:15 +0530

    setup

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 export const Items = () => {
   return (
-    <div>
-    <p>12 August, 2023</p>
-    <h3>Smart Phone</h3>
-    <p>$248</p>
-    </div>
-  )
+		<div>
+			<p>12 August, 2023</p>
+			<h3>Smart Phone</h3>
+			<p>$248</p>
+		</div>
+	);
 }
