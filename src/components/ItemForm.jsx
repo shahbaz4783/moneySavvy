d4cc908634276a618e705d5e6e210f2600608627
shahbaz4783 <shahbaz4783@gmail.com>
2023-09-14 22:30:37 +0530

    form submission

--- src/components/ItemForm.jsx
+++ src/components/ItemForm.jsx
@@ -9,22 +9,47 @@ export const ItemForm = () => {
 		setItem(e.target.value);
 	};
 	const dateHandler = (e) => {
-		setPrice(e.target.value);
+		setDate(e.target.value);
 	};
 	const priceHandler = (e) => {
-		setDate(e.target.value);
+		setPrice(e.target.value);
+	};
+
+  const submitHandler = (e) => {
+		e.preventDefault();
+		const data = {
+			item: item,
+			price: price,
+			date: new Date(date),
+		};
+		console.log(data);
+
+		setItem('');
+		setPrice('');
+		setDate('');
 	};
 
 	return (
-		<form>
-			<input onChange={itemHandler} type='text' placeholder='Enter Item Name' />
-			<input onChange={priceHandler} type='number' placeholder='Enter Amount' />
+		<form onSubmit={submitHandler}>
+			<input
+				onChange={itemHandler}
+				value={item}
+				type='text'
+				placeholder='Enter Item Name'
+			/>
+			<input
+				onChange={priceHandler}
+				value={price}
+				type='number'
+				placeholder='Enter Amount'
+			/>
 			<input
 				onChange={dateHandler}
 				type='date'
 				min={'2016-01-01'}
 				max={'2023-12-31'}
 				placeholder='Choose Date'
+				value={date}
 			/>
 			<button type='submit'>Add</button>
 		</form>
