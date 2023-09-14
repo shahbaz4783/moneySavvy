2e7d3300fa7a5d32935d313151ce8c8107e5be5e
shahbaz4783 <shahbaz4783@gmail.com>
2023-09-14 21:42:44 +0530

    multiple states

--- src/components/ItemForm.jsx
+++ src/components/ItemForm.jsx
@@ -1,9 +1,26 @@
+import { useState } from 'react';
+
 export const ItemForm = () => {
+	const [item, setItem] = useState('');
+	const [price, setPrice] = useState('');
+	const [date, setDate] = useState('');
+
+	const itemHandler = (e) => {
+		setItem(e.target.value);
+	};
+	const dateHandler = (e) => {
+		setPrice(e.target.value);
+	};
+	const priceHandler = (e) => {
+		setDate(e.target.value);
+	};
+
 	return (
 		<form>
-			<input type='text' placeholder='Enter Item Name' />
-			<input type='number' placeholder='Enter Amount' />
+			<input onChange={itemHandler} type='text' placeholder='Enter Item Name' />
+			<input onChange={priceHandler} type='number' placeholder='Enter Amount' />
 			<input
+				onChange={dateHandler}
 				type='date'
 				min={'2016-01-01'}
 				max={'2023-12-31'}
