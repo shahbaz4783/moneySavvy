12e651da801ecf46b52894ae68a4c0a29676ba32
shahbaz4783 <shahbaz4783@gmail.com>
2023-09-16 23:28:51 +0530

    data in ibh

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
 import { Card } from './Card';
+
+const items = [
+	{
+		id: 1,
+		date: '12 Jan, 2023',
+		item: 'Laptop',
+		price: '$850',
+	},
+	{
+		id: 2,
+		date: '23 Mar, 2023',
+		item: 'Monitor',
+		price: '$40',
+	},
+	{
+		id: 3,
+		date: '14 June, 2023',
+		item: 'Keyboard',
+		price: '$150',
+	},
+];
 
 export const Container = () => {
 	return (
 		<>
-			<Card date={'12 Jan, 2023'} item={'Laptop'} price={'$850'} />
-			<Card date={'23 Mar, 2023'} item={'Mobile'} price={'$250'} />
-			<Card date={'14 June, 2023'} item={'Keyboard'} price={'$30'} />
+			<Card date={items[0].date} item={items[0].item} price={items[0].price} />
+			<Card date={items[1].date} item={items[1].item} price={items[1].price} />
+			<Card date={items[2].date} item={items[2].item} price={items[2].price} />
 		</>
 	);
 };
