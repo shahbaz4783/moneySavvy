56c5863bc93b516f768b1288cf861b868345accc
shahbaz4783 <shahbaz4783@gmail.com>
2023-09-22 23:40:33 +0530

    form styling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,9 @@
 export const Card = ({ date, item, price }) => {
 
   return (
-		// <div className='card'>
-		// 	<p>{date}</p>
-		// 	<h3>{item}</h3>
-		// 	<p>{price}</p>
-		<div class='task' draggable='true'>
+		<div class='task'>
 			<div class='tags'>
-				<span class='tag'>Electronics</span>
+				<span class='tag'>{date}</span>
 			</div>
 			<h3>{item}</h3>
 			<p>
@@ -15,33 +11,9 @@ export const Card = ({ date, item, price }) => {
 			</p>
 			<div class='stats'>
 				<div>
-					<div>
-						<svg
-							xmlns='http://www.w3.org/2000/svg'
-							fill='none'
-							viewBox='0 0 24 24'
-						>
-							<g stroke-width='0' id='SVGRepo_bgCarrier'></g>
-							<g
-								stroke-linejoin='round'
-								stroke-linecap='round'
-								id='SVGRepo_tracerCarrier'
-							></g>
-							<g id='SVGRepo_iconCarrier'>
-								{' '}
-								<path
-									stroke-linecap='round'
-									stroke-width='2'
-									d='M12 8V12L15 15'
-								></path>{' '}
-								<circle stroke-width='2' r='9' cy='12' cx='12'></circle>{' '}
-							</g>
-						</svg>
-						{date}
-					</div>
+					<h2>{price}</h2>
 				</div>
 			</div>
 		</div>
-		// </div>
 	);
 }
--- src/components/ItemForm.jsx
+++ src/components/ItemForm.jsx
@@ -43,31 +43,77 @@ export const ItemForm = ({ onSaveItemData , onCancel}) => {
 	};
 
 	return (
-		<form onSubmit={submitHandler}>
-			<input
-				style={{ background: !isValid && 'pink' }}
-				onChange={itemHandler}
-				value={item}
-				type='text'
-				placeholder='Enter Item Name'
-			/>
-			<input
-				className={`${!isValid && 'invalid'}`}
-				onChange={priceHandler}
-				value={price}
-				type='number'
-				placeholder='Enter Amount'
-			/>
-			<input
-				onChange={dateHandler}
-				type='date'
-				min={'2016-01-01'}
-				max={'2023-12-31'}
-				placeholder='Choose Date'
-				value={date}
-			/>
-			<button onClick={onCancel}>Cancel</button>
-			<button type='submit'>Add</button>
-		</form>
+		<>
+			<form onSubmit={submitHandler}>
+				<div class='container'>
+					<div class='modal'>
+						<div class='modal__header'>
+							<span class='modal__title'>New project</span>
+							<button class='button button--icon'>
+								<svg
+									width='24'
+									viewBox='0 0 24 24'
+									height='24'
+									xmlns='http://www.w3.org/2000/svg'
+									onClick={onCancel}
+								>
+									<path fill='none' d='M0 0h24v24H0V0z'></path>
+									<path d='M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z'></path>
+								</svg>
+							</button>
+						</div>
+						<div class='modal__body'>
+							<div class='input'>
+								<label class='input__label'>Item Name</label>
+								<input
+									class='input__field'
+									type='text'
+									onChange={itemHandler}
+									value={item}
+								/>
+								<p class='input__description'>
+									The title must contain a maximum of 32 characters
+								</p>
+							</div>
+							<div class='input'>
+								<label class='input__label'>Enter Amount</label>
+								<input
+									class='input__field'
+									type='number'
+									onChange={priceHandler}
+									value={price}
+								/>
+								<p class='input__description'>
+									The title must contain a maximum of 32 characters
+								</p>
+							</div>
+							<div class='input'>
+								<label class='input__label'>Enter Date</label>
+								<input
+									class='input__field'
+									type='date'
+									onChange={dateHandler}
+									value={date}
+								/>
+								<p class='input__description'>
+									The title must contain a maximum of 32 characters
+								</p>
+							</div>
+							<div class='input'>
+								<label class='input__label'>Description</label>
+								<textarea class='input__field input__field--textarea'></textarea>
+								<p class='input__description'>
+									Give your project a good description so everyone know what's
+									it for
+								</p>
+							</div>
+						</div>
+						<div class='modal__footer'>
+							<button class='button button--primary'>Add</button>
+						</div>
+					</div>
+				</div>
+			</form>
+		</>
 	);
 };
