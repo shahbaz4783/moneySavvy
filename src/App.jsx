3898e7fd15718c64859c79f85131d0be192ba1bc
shahbaz4783 <shahbaz4783@gmail.com>
2023-09-23 23:19:45 +0530

    more items added

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,13 @@ import { FormModal } from './components/FormModal'
 const DUMMY_EXPENSE = [
 	{
 		id: 1,
-		date: new Date('12 Jan, 2023'),
+		date: new Date('12 Jan, 2019'),
 		name: 'Laptop',
 		price: '$850',
 	},
 	{
 		id: 2,
-		date: new Date('23 Mar, 2022'),
+		date: new Date('23 Mar, 2021'),
 		name: 'Monitor',
 		price: '$40',
 	},
@@ -20,6 +20,30 @@ const DUMMY_EXPENSE = [
 		date: new Date('14 June, 2021'),
 		name: 'Keyboard',
 		price: '$150',
+	},
+	{
+		id: 4,
+		date: new Date('13 April, 2022'),
+		name: 'WebCam',
+		price: '$150',
+	},
+	{
+		id: 5,
+		date: new Date('16 March, 2023'),
+		name: 'Keyboard',
+		price: '$150',
+	},
+	{
+		id: 6,
+		date: new Date('27 January, 2023'),
+		name: 'Earbuds',
+		price: '$50',
+	},
+	{
+		id: 7,
+		date: new Date('14 June, 2023'),
+		name: 'Pendrive',
+		price: '$20',
 	},
 ];
 
